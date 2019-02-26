d2ff6539eaf3c2dbeedf6772c033a3305b4ef2a1
quietdw <1044569102@qq.com>
2019-02-26 19:48:12 +0800

    项目建立

---
+++ src/pages/Login/template.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="login">
+    登录页面
+  </div>
+</template>
+
+<script src='./template.js'></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src='./template.css'></style>
