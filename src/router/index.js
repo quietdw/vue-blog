cd897ae7cd1e53ee203bc6283f5d2f2a0e582d99
quietdw <1044569102@qq.com>
2019-03-04 21:08:18 +0800

    优化加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,54 +1,94 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/pages/Index/template.vue'
-import User from '@/pages/User/template.vue'
-import Register from '@/pages/Register/template.vue'
-import Edit from '@/pages/Edit/template.vue'
-import My from '@/pages/My/template.vue'
-import Detail from '@/pages/Detail/template.vue'
-import Create from '@/pages/Create/template.vue'
-import Login from '@/pages/Login/template.vue'
+// import Index from '@/pages/Index/template.vue'
+// import User from '@/pages/User/template.vue'
+// import Register from '@/pages/Register/template.vue'
+// import Edit from '@/pages/Edit/template.vue'
+// import My from '@/pages/My/template.vue'
+// import Detail from '@/pages/Detail/template.vue'
+// import Create from '@/pages/Create/template.vue'
+// import Login from '@/pages/Login/template.vue'
 import store from '@/store/'
 
 Vue.use(Router)
+
+// const router = new Router({
+//   routes: [{
+//     path: '/',
+//     component: Index
+//   },
+//   {
+//     path: '/login',
+//     component: Login
+//   },
+//   {
+//     path: '/Register',
+//     component: Register
+//   },
+//   {
+//     path: '/Index',
+//     component: Index
+//   },
+//   {
+//     path: '/User',
+//     component: User
+//   },
+//   {
+//     path: '/Edit/:blogId',
+//     component: Edit,
+//     meta: {requiresAuth: true}
+//   },
+//   {
+//     path: '/My',
+//     component: My,
+//     meta: {requiresAuth: true}
+//   },
+//   {
+//     path: '/Detail/:blogId',
+//     component: Detail
+//   },
+//   {
+//     path: '/Create',
+//     component: Create,
+//     meta: {requiresAuth: true}
+//   }
+//   ]
+// })
+
 const router = new Router({
   routes: [{
     path: '/',
-    component: Index
+    component: () => import ('@/pages/Index/template.vue') 
   },
   {
     path: '/login',
-    component: Login
+    component: () => import('@/pages/Login/template.vue')
   },
   {
     path: '/Register',
-    component: Register
-  },
-  {
-    path: '/Index',
-    component: Index
+    component: () => import('@/pages/Register/template.vue')
   },
   {
     path: '/User',
-    component: User
+    component: () => import('@/pages/User/template.vue')
   },
   {
     path: '/Edit/:blogId',
-    component: Edit,
+    component: () => import('@/pages/Edit/template.vue'),
     meta: {requiresAuth: true}
   },
   {
     path: '/My',
-    component: My,
+    component: () => import('@/pages/My/template.vue'),
     meta: {requiresAuth: true}
   },
   {
     path: '/Detail/:blogId',
-    component: Detail
+    component: () => import('@/pages/Detail/template.vue')
   },
   {
     path: '/Create',
-    component: Create,
+    component: () => import('@/pages/Create/template.vue'),
     meta: {requiresAuth: true}
   }
   ]
