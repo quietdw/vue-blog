b161bac674f814d67f826a2db132b4db76649e5c
quietdw <1044569102@qq.com>
2019-03-06 23:32:39 +0800

    创建页面增加提示信息

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -13,6 +13,7 @@ export default {
     onSubmit () {
       blog.createBlog({title: this.title, description: this.description, content: this.content, atIndex: this.atIndex})
         .then(res => {
+          this.$message('创建成功');
           this.$router.push({path: `/detail/${res.data.id}`})
         })
     }
