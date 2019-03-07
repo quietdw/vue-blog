256883bd2493ce5799fda37cae74a484f99a0450
quietdw <1044569102@qq.com>
2019-03-07 11:54:13 +0800

    修改页面的标题和简介限制字数

--- src/pages/Edit/template.js
+++ src/pages/Edit/template.js
@@ -6,26 +6,51 @@ export default {
       title: '',
       description: '',
       content: '',
-      atIndex: false
+      atIndex: false,
+      isActive: false,
+      isActive1: false
     }
   },
   created () {
+    this.titleLength = this.title.length
+    this.descriptionLength = this.description.length
     let blogId = this.$route.params.blogId
     blog.getDetail(blogId)
-    .then(res => {
+      .then(res => {
         this.title = res.data.title
         this.description = res.data.description
         this.content = res.data.content
         this.atIndex = res.data.atIndex
-    })
+      })
+  },
+  beforeUpdate () {
+    this.titleLength = this.title.length
+    if (this.titleLength >= 30) {
+      this.isActive = true
+    } else {
+      this.isActive = false
+    }
+    this.descriptionLength = this.description.length
+    if (this.descriptionLength >= 100) {
+      this.isActive1 = true
+    } else {
+      this.isActive1 = false
+    }
   },
   methods: {
     onEdit () {
-      blog.updateBlog({blogId:this.$route.params.blogId},{title: this.title, description: this.description, content: this.content, atIndex: this.atIndex})
-        .then(res => {
-          this.$message('更改成功');
-          this.$router.push({path: `/detail/${res.data.id}`})
+      if (!this.isActive && !this.isActive1) {
+        blog.updateBlog({blogId: this.$route.params.blogId},{title: this.title, description: this.description, content: this.content, atIndex: this.atIndex})
+          .then(res => {
+            this.$message('更改成功')
+            this.$router.push({path: `/detail/${res.data.id}`})
+          })
+      } else {
+        this.$message({
+          message: '标题或者简介过长',
+          type: 'warning'
         })
-    }
+      }
+    } 
   }
 }
--- src/pages/Edit/template.vue
+++ src/pages/Edit/template.vue
@@ -1,14 +1,13 @@
 <template>
   <div id="edit">
     <h4>标题</h4>
-    <el-input  placeholder="请输入标题" v-model="title"></el-input>
-    <p class="message">限30个字</p>
+    <el-input  placeholder="请输入标题" v-model="title" v-bind:class="{ active: isActive }"></el-input>
+    <p class="message" v-bind:class="{ active: isActive }">限{{titleLength}}/30个字</p>
     <h4>简介</h4>
-    <el-input  type="textarea" :rows="2" v-model="description" :autosize='{minRows:2,maxRows:4}'></el-input>
-    <p class="message">限30个字</p>
+    <el-input  type="textarea" :rows="2" v-model="description" :autosize='{minRows:2,maxRows:4}' v-bind:class="{ active1: isActive1 }"></el-input>
+    <p class="message" v-bind:class="{ active1: isActive1 }">限{{descriptionLength}}/100个字</p>
     <h4>内容</h4>
     <el-input  type="textarea" :rows="20" v-model="content" :autosize='{minRows:4,maxRows:20}'></el-input>
-    <p class="message">限30个字</p>
     <p class="switch">是否展示到首页<el-switch　v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch></p>
     <el-button type="primary" plain @click="onEdit">修改并提交</el-button>
   </div>
