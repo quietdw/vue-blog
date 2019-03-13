646368ec849835e578073156c00789ee1010c4fb
quietdw <1044569102@qq.com>
2019-03-14 00:33:06 +0800

    未登录时的header以及footer样式修改

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,10 @@
 <template>
     <footer>
-        <p>@Copyright Jiang, OpenSource: <a href="https://github.com/quietdw/vue-blog">GitHub</a></p>
+        <p>© 2019 Jiang OpenSource:<a href="https://github.com/quietdw/vue-blog">GitHub</a>
+            <a href="https://github.com/quietdw/vue-blog"><svg class="icon" aria-hidden="true">
+    <use xlink:href="#icon-github"></use>
+</svg></a>
+        </p>
     </footer>
 </template>
 
@@ -10,15 +14,27 @@
 footer {
     padding: 10px;
   align-self: end;
-  background-color: #d7d7d7;
+  background-color: #fafbfc;
   color:#666;
   font-size: 13px;
-  padding: 10px;
-  text-align: center;
+  padding:  24px 16px;
+  text-align: left;
   p {
       a{
-          color: #333
+              svg{
+          width: 2em;
+          height: 2em;
+          fill: #959da5;
       }
+      }
+      a:hover{
+          color: #0366d6;
+          text-decoration: underline;
+      }
+      a:nth-child(2){
+          float: right;
+      }
+  
   }
 }
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,109 +1,156 @@
 <template>
-    <header :class="{login:isLogin,nologin:!isLogin}">
-        <template v-if="!isLogin">
-            <h1><router-link to="/">Let's share</router-link></h1>
-            <p>精品博客汇聚</p>
-            <div class="btns">
-                <router-link to="/login"><el-button>登录</el-button></router-link>
-                <router-link to="/register"><el-button>注册</el-button></router-link>
-            </div>
-        </template>
-        <template v-if="isLogin">
-            <h1><router-link to="/">Let's share</router-link></h1>
-            <router-link to="/create">
-                <svg class="icon" aria-hidden="true">
-                    <use xlink:href="#icon-zengjia"></use>
-                </svg>
-            </router-link>
-            <div class="user">
-                <img :src="user.avatar" :alt="user.username">
-                <ul>
-                    <li><router-link to="/my">我的</router-link></li>
-                    <li><a href="#" @click="onLogout">注销</a></li>
-                </ul>
-            </div>
-        </template>
-    </header>
+  <header :class="{login:isLogin,nologin:!isLogin}">
+    <template v-if="!isLogin">
+      <h1>
+        <router-link to="/">Build better together</router-link>
+      </h1>
+      <div class="btns">
+        <router-link to="/login">
+          <el-button>Sign in</el-button>
+        </router-link>
+        <router-link to="/register">
+          <el-button>Sign up</el-button>
+        </router-link>
+      </div>
+    </template>
+    <template v-if="isLogin">
+      <h1>
+        <router-link to="/">Let's share</router-link>
+      </h1>
+      <router-link to="/create">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-zengjia"></use>
+        </svg>
+      </router-link>
+      <div class="user">
+        <img :src="user.avatar" :alt="user.username">
+        <ul>
+          <li>
+            <router-link to="/my">我的</router-link>
+          </li>
+          <li>
+            <a href="#" @click="onLogout">注销</a>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </header>
 </template>
 
 <script>
-/* eslint-disable */ 
+/* eslint-disable */
+
 //获取vuex里的数据
-import auth from '../api/auth'
-import {mapGetters, mapActions} from 'vuex'
+import auth from "../api/auth";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-    data () {
-        return {}
-    },
-    created(){ // 生命周期，数据OK，模板还未渲染，比如ajax获取数据
-        this.checkLogin()
+  data() {
+    return {};
+  },
+  created() {
+    // 生命周期，数据OK，模板还未渲染，比如ajax获取数据
+    this.checkLogin();
+  },
+  computed: {
+    ...mapGetters(["user", "isLogin"])
+  },
+  methods: {
+    ...mapActions(["checkLogin", "logout"]),
+    onLogout() {
+      this.logout();
     }
-    ,
-    computed:{
-         ...mapGetters([
-            'user',
-            'isLogin'
-         ])
-    }
-    ,
-    methods:{
-        ...mapActions([
-            'checkLogin',
-            'logout'
-        ]),
-        onLogout(){
-            this.logout()
-        }
-    }
-
-}
+  }
+};
 </script>
 
 <style lang="less">
 @import url(../assets/base.less);
 
-header{background-color: @bgColor;padding-bottom: 30px;display: grid;color: #fff;
-    h1 {text-transform: uppercase;font-weight: bold;}
+header {
+  background-color: @bgColor;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  display: grid;
+  grid: auto / 1fr 160px;
+  color: #fff;
+  h1 {
+    font-weight: bold;
+  }
 }
 
 header.nologin {
-    color: #fff;
-  h1 {padding: 20px 0;}
-  p {padding-bottom:  10px;}
-  .btns {margin-top: 20px;
-    .el-button{
-        margin: 20px;
+  color: #fff;
+  h1 {
+      text-align: left;
+  }
+  .btns {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    a {
+        .el-button {
+        padding: 8px 10px;
+        font-size: 16px;
+            transition: .4s;
+        }
+    }
+    a:nth-child(1) {
+      .el-button {
+          padding:2px;
+        background: @bgColor;
+        color: #fff;
+        border: none;
+
+      }
+    }
+     a:nth-child(2) {
+      .el-button {
+        background: @bgColor;
+        color: #fff;
+        border: solid 1px #fff;
+      }
+    }
+    .el-button:nth-child(1):focus,
+    .el-button:nth-child(1):hover {
+      color: hsla(0, 0%, 100%, 0.75);
     }
   }
 }
 header.login {
-    display: flex;
-    align-items:center;
-    justify-content:flex-end;
-    padding-bottom: 0px;
-    h1 {flex-grow: 1;text-align: left;}
-    span {color: #fff;}
-    svg {
-        width: 30px;
-        height: 30px;
-        fill: aliceblue;
-    }
-    .user {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 0px;
+  h1 {
+    flex-grow: 1;
+    text-align: left;
+  }
+  span {
+    color: #fff;
+  }
+  svg {
+    width: 30px;
+    height: 30px;
+    fill: aliceblue;
+  }
+  .user {
     position: relative;
     margin-right: 15px;
-    img {width: 40px;margin: 10px;}
-    ul{
-        position: absolute;
-        width: 100%;
-        left: 100%;
-        top: 10px;
-        li{
-            color: #fff;
-
-        }
+    img {
+      width: 40px;
+      margin: 10px;
     }
+    ul {
+      position: absolute;
+      width: 100%;
+      left: 100%;
+      top: 10px;
+      li {
+        color: #fff;
+      }
     }
+  }
 }
 </style>
 
