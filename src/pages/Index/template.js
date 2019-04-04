2dfd022940192f2a4337fda2e92808a9f14b4eb6
quietdw <1044569102@qq.com>
2019-04-05 00:23:44 +0800

    blog.user 为 null 时会报错

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -3,28 +3,47 @@ import auth from '@/api/auth.js'
 import blog from '@/api/blog.js'
 
 export default {
-  data () {
+  data() {
     return {
       blogs: [],
       total: 0,
       currentPage: 0
     }
   },
-  created () {
+  created() {
     let currentPage = parseInt(this.$route.query.page, 10) || ''
-    blog.getBlogs({page: currentPage}).then(res => {
+    blog.getBlogs({
+      page: currentPage
+    }).then(res => {
+      res.data.forEach((blog, key) => {
+        if (!blog.user) {
+          res.data.splice(key)
+        }
+      });
       this.blogs = res.data
       this.total = res.total
       this.currentPage = res.page
     })
   },
   methods: {
-    onPageChange (newPage) {
-      blog.getBlogs({page: newPage}).then(res => {
+    onPageChange(newPage) {
+      blog.getBlogs({
+        page: newPage
+      }).then(res => {
+        res.data.forEach((blog, key) => {
+          if (!blog.user) {
+            res.data.splice(key)
+          }
+        });
         this.blogs = res.data
         this.total = res.total
         this.currentPage = res.page
-        this.$router.push({path: '/', query: {page: newPage}})
+        this.$router.push({
+          path: '/',
+          query: {
+            page: newPage
+          }
+        })
       })
     }
   }
